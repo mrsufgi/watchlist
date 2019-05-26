b0647ea6b652e72b7debba84c892ee99b0eb91f7
Ori Avraham <sufgis@gmail.com>
2019-05-26 19:27:14 +0300

    bigger watchlist

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -4,7 +4,7 @@ import WatchListItem from './WatchListItem';
 import useFetch from '../useFetch';
 import { LoggedInContext } from '../App';
 
-const refereshInterval = 5000; // 300000;
+const refereshInterval = 300000;
 
 function useUserList() {
   const [userItems, setUserItems] = useState([]);
@@ -61,8 +61,12 @@ function useTokenList() {
   const getTokensListApi = () => {
     return Promise.resolve({
       data: [
-        { tokenId: '0', name: 'btc', price: '100$' },
-        { tokenId: '1', name: 'eth', price: '10$' },
+        { tokenId: '0', name: 'btc', price: '10000$' },
+        { tokenId: '1', name: 'eth', price: '100$' },
+        { tokenId: '2', name: 'ltc', price: '200$' },
+        { tokenId: '3', name: 'sc', price: '0.1$' },
+        { tokenId: '4', name: 'dgb', price: '0.2$' },
+        { tokenId: '5', name: 'etc', price: '2$' },
       ],
     });
   };
@@ -87,7 +91,7 @@ function useTokenList() {
     getTokens,
   };
 }
-
+// TODO:// create Auth Component
 function WatchList() {
   const { tokens, getTokens } = useTokenList();
   const { userItems, addUserItem, removeUserItem } = useUserList();
